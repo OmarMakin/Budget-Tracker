939ee276b8b2c2a635f0bc840878535bf6dd39ec
Omar Makin <omar.makin@hotmail.com>
2025-02-04 15:04:06 +0200

    Add reports page

---
+++ src/app/pages/reports/reports.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
+import { Chart } from 'chart.js/auto';
+
+interface Transaction {
+  date: string;
+  category: string;
+  amount: number;
+  type: 'income' | 'expense';
+}
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './reports.component.html',
+  styleUrls: ['./reports.component.css'],
+  standalone: false
+})
+export class ReportsComponent implements OnInit {
+  transactions: Transaction[] = [];
+  filteredTransactions: Transaction[] = [];
+  categoryFilter = '';
+  dateFilter = '';
+  chart: any;
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit() {
+    this.transactions = this.transactionService.getTransactions();
+    this.filteredTransactions = [...this.transactions];
+  }
+
+  applyFilters() {
+    this.filteredTransactions = this.transactions.filter(transaction => {
+      const matchesCategory = this.categoryFilter
+        ? transaction.category.toLowerCase().includes(this.categoryFilter.toLowerCase())
+        : true;
+
+      const matchesDate = this.dateFilter
+        ? transaction.date === this.dateFilter
+        : true;
+
+      return matchesCategory && matchesDate;
+    });
+  }
+
+}
